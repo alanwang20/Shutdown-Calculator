47c51ae24512c6bbc7885d6f32f4ac0133e0b574
Alan Wang <alanwang2020@gmail.com>
2021-05-10 23:58:04 -0400

    first commit

---
+++ src/Shutdown.py
@@ -0,0 +1,49 @@
+from __future__ import division
+from sympy import *
+
+class Shutdown :
+    
+    x, y, z = symbols('x y z')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # cost_string = "((1/3) * q**2 + 50*q +2000)"
+    # p, q = symbols('p q')
+    # cost_expr = sympify(cost_string)
+    # #print(cost_expr)
+    
+    # #multiply cost_expr by -1 
+    # cost_expr = Mul(-1, cost_expr)
+
+    # temp = Mul(p,q)
+    # print(temp)
+
+    # # profit_exp = "p*q -"
+    # # profit_function = profit_exp + cost_string
+    
+    # # profit_expr = sympify(profit_function)
+    # # profit_expr = expand(profit_expr)
+    # # print(profit_function)
+
+    # # #Take first order conditions, calculate partial derivative of q
+    
+    # # diff(profit_expr, q)
+    # # print("partical derivative: " + profit_exp)
