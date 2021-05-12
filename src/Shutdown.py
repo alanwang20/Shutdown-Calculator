f048d63f51118fecc412cd9c8f50c9e09f8db5bd
Alan Wang <alanwang2020@gmail.com>
2021-05-12 18:26:27 -0400

    worked up to trying to switch ATC q for q_s of individual firm

--- src/Shutdown.py
+++ src/Shutdown.py
@@ -5,45 +5,43 @@ class Shutdown :
     
     x, y, z = symbols('x y z')
     
+   
+    #create profit funciton
+    cost_string = "((1/3) * q**3 + 50*q +2000)"
+    p, q = symbols('p q')
+    cost_expr = sympify(cost_string)
+    # print(cost_expr)
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    # cost_string = "((1/3) * q**2 + 50*q +2000)"
-    # p, q = symbols('p q')
-    # cost_expr = sympify(cost_string)
-    # #print(cost_expr)
-    
-    # #multiply cost_expr by -1 
-    # cost_expr = Mul(-1, cost_expr)
+    # multiply cost_expr by -1 
+    cost_expr = Mul(-1, cost_expr)
 
-    # temp = Mul(p,q)
-    # print(temp)
-
-    # # profit_exp = "p*q -"
-    # # profit_function = profit_exp + cost_string
+    cost_exp = "p*q -"
+    profit_function = cost_exp + cost_string
     
-    # # profit_expr = sympify(profit_function)
-    # # profit_expr = expand(profit_expr)
-    # # print(profit_function)
+    profit_expr = sympify(profit_function)
+    profit_expr = simplify(profit_expr)
+    print(profit_function)
 
-    # # #Take first order conditions, calculate partial derivative of q
+    # take first order conditions, calculate partial derivative of q
     
-    # # diff(profit_expr, q)
-    # # print("partical derivative: " + profit_exp)
+    foc_expr = diff(profit_expr, q)
+    print("partical derivative: " )
+    print (foc_expr)
+
+
+    # find firm supply function
+    results = solve(foc_expr,q) #this will give a pos and neg function
+    firm_supply = results[1] #this isolates positive function
+    print(firm_supply)
+
+    # calculate ATC
+
+    #calculate averagetotal cost function
+    atc = diff(cost_expr, q)
+    print("ATC")
+    print (firm_supply)
+
+    #substitute "q" value in atc for firm_supply
+    adjusted_atc = atc.replace(firm_supply,p)
+    print("Adjusted ATC")
+    print(adjusted_atc)
